3b825a6962f0fb64258e6137fb74dd9a6ed0d3f5
Liona <lionafc@gmail.com>
2015-11-14 21:10:17 -0500

    add to repo

---
+++ GeigerCounts/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace GeigerCounts
+{
+    partial class GeigerCounter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startButton = new System.Windows.Forms.Button();
+            this.endButton = new System.Windows.Forms.Button();
+            this.runTime = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.output = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fileName = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.tickerTimer = new System.Windows.Forms.Timer(this.components);
+            this.countDown = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comportSelect = new System.Windows.Forms.ComboBox();
+            this.durationTimer = new System.Windows.Forms.Timer(this.components);
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(152, 317);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(132, 59);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_click);
+            // 
+            // endButton
+            // 
+            this.endButton.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.endButton.Location = new System.Drawing.Point(741, 357);
+            this.endButton.Name = "endButton";
+            this.endButton.Size = new System.Drawing.Size(132, 59);
+            this.endButton.TabIndex = 1;
+            this.endButton.Text = "Abort";
+            this.endButton.UseVisualStyleBackColor = true;
+            this.endButton.Click += new System.EventHandler(this.endButton_Click);
+            // 
+            // runTime
+            // 
+            this.runTime.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.runTime.Location = new System.Drawing.Point(60, 242);
+            this.runTime.Name = "runTime";
+            this.runTime.Size = new System.Drawing.Size(132, 26);
+            this.runTime.TabIndex = 2;
+            this.runTime.Text = "00:00:20";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 220);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Run Time (HH:MM:SS)";
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(460, 56);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(413, 270);
+            this.output.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(456, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Output";
+            // 
+            // fileName
+            // 
+            this.fileName.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileName.Location = new System.Drawing.Point(60, 162);
+            this.fileName.Name = "fileName";
+            this.fileName.Size = new System.Drawing.Size(261, 26);
+            this.fileName.TabIndex = 6;
+            this.fileName.Text = "C:\\Users\\Liona\\Desktop\\test.txt";
+            // 
+            // browseButton
+            // 
+            this.browseButton.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.browseButton.Location = new System.Drawing.Point(327, 158);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 30);
+            this.browseButton.TabIndex = 7;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(56, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Save Output To";
+            // 
+            // tickerTimer
+            // 
+            this.tickerTimer.Interval = 1000;
+            this.tickerTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // countDown
+            // 
+            this.countDown.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countDown.Location = new System.Drawing.Point(460, 379);
+            this.countDown.Name = "countDown";
+            this.countDown.Size = new System.Drawing.Size(132, 26);
+            this.countDown.TabIndex = 9;
+            this.countDown.Text = "00:00:00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(456, 357);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 19);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Time Left";
+            // 
+            // comportSelect
+            // 
+            this.comportSelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comportSelect.FormattingEnabled = true;
+            this.comportSelect.Location = new System.Drawing.Point(60, 90);
+            this.comportSelect.Name = "comportSelect";
+            this.comportSelect.Size = new System.Drawing.Size(185, 21);
+            this.comportSelect.TabIndex = 11;
+            this.comportSelect.SelectedIndexChanged += new System.EventHandler(this.comportSelect_SelectedIndexChanged);
+            // 
+            // durationTimer
+            // 
+            this.durationTimer.Tick += new System.EventHandler(this.durationTimer_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(56, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 19);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Read Port";
+            // 
+            // GeigerCounter
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 459);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comportSelect);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.countDown);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.fileName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.runTime);
+            this.Controls.Add(this.endButton);
+            this.Controls.Add(this.startButton);
+            this.Name = "GeigerCounter";
+            this.Text = "Gieger Counter";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button endButton;
+        private System.Windows.Forms.TextBox runTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox fileName;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Timer tickerTimer;
+        private System.Windows.Forms.TextBox countDown;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comportSelect;
+        private System.Windows.Forms.Timer durationTimer;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ GeigerCounts/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeigerCounts
+{
+    public partial class GeigerCounter : Form
+    {
+        Thread thread1 = null;
+        private TimeSpan timeLeft;
+        string selectedPortName = "COM3";
+        string[] ports = SerialPort.GetPortNames();
+        int counts = 0;
+
+
+        public GeigerCounter()
+        {
+            InitializeComponent();
+            comportSelect.Items.AddRange(ports);
+        }
+
+
+        private void endButton_Click(object sender, EventArgs e)
+        {
+
+            Environment.Exit(0);
+        }
+
+        private void startButton_click(object sender, EventArgs e)
+        {
+            if (durationTimer.Enabled == false)
+            {
+                durationTimer.Start();
+                if (thread1 == null || !thread1.IsAlive)
+                {
+                    thread1 = new Thread(this.StartCounterThread);
+                    durationTimer.Interval = (int)TimeSpan.Parse(runTime.Text).TotalMilliseconds;
+                    thread1.Start();
+                }
+                startButton.Text = "Stop";
+                tickerTimer.Enabled = true;
+                timeLeft = TimeSpan.Parse(runTime.Text);
+            }
+            else
+            {
+
+                startButton.Text = "Start";
+                countDown.Text = "00:00:00";
+                tickerTimer.Stop();
+                durationTimer.Stop();
+            }
+        }
+
+        private void StartCounterThread()
+        {
+            SerialPort ComPort = new SerialPort(selectedPortName);
+            ComPort.Open();
+
+            using (System.IO.StreamWriter fs = System.IO.File.AppendText(this.fileName.Text))
+            {
+                fs.WriteLine(DateTime.Now);
+                fs.Close();
+            }
+
+            while (durationTimer.Enabled)
+            {
+                int num = (int)ComPort.ReadChar();
+                if ((num==48)|| (num==49))
+                    { counts++;}
+            }
+            ComPort.Close();
+        }
+
+        delegate void AddTextCallback(string text);
+         
+        private void AddText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.output.InvokeRequired)
+            {
+                AddTextCallback d = new AddTextCallback(AddText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.output.AppendText(text);
+            }
+        }
+
+        private void SetText(string text)
+        {
+            if (this.output.InvokeRequired)
+            {
+                AddTextCallback d = new AddTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                startButton.Text = text;
+            }
+        }
+
+        private void browseButton_Click(object sender, System.EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.fileName.Text = saveFileDialog1.FileName;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeLeft = timeLeft - TimeSpan.FromMilliseconds(tickerTimer.Interval);
+            countDown.Text = timeLeft.ToString(@"dd\.hh\:mm\:ss");
+            using (System.IO.StreamWriter fs = System.IO.File.AppendText(this.fileName.Text))
+            {
+                fs.WriteLine(counts);
+            }
+            AddText($"{counts} \n");
+            counts = 0;
+        }
+
+        private void durationTimer_Tick(object sender, EventArgs e)
+        {
+                startButton.Text = "Start";
+                countDown.Text = "00:00:00";
+                tickerTimer.Stop();
+                durationTimer.Stop();           
+        }
+
+        private void comportSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedPortName = (string)comportSelect.SelectedItem;
+        }
+
+    }
+}
